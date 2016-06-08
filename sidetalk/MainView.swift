b30edda4a731e88c65466696f361335143f6a09e
Clint Tseng <clint@dontexplain.com>
2016-06-07 23:23:11 -0700

    bug: do drawing on main UI thread to fix weird delay.

--- sidetalk/MainView.swift
+++ sidetalk/MainView.swift
@@ -19,22 +19,22 @@ class MainView: NSView {
     }
 
     private func redraw(contacts: [Contact]) {
-        NSLog("redrawing with \(contacts.count) contacts")
-        // first nuke the old stuff (TODO: eventually relocate+anim)
+        dispatch_async(dispatch_get_main_queue(), {
+            // first nuke the old stuff (TODO: eventually relocate+anim)
 
-        // now draw new stuff
-        let origin = NSPoint(x: 100, y: 500);
-        let tileSize = NSSize(width: 200, height: 50);
-        let padding = CGFloat(4);
+            // now draw new stuff
+            let origin = NSPoint(x: 100, y: 500);
+            let tileSize = NSSize(width: 200, height: 50);
+            let padding = CGFloat(4);
 
-        for (idx, contact) in contacts.enumerate() {
-            let newTile = ContactTile(
-                frame: NSRect(origin: NSPoint(x: origin.x, y: origin.y - ((tileSize.height + padding) * CGFloat(idx))), size: tileSize),
-                contact: contact
-            );
-            self.addSubview(newTile);
-        }
-        NSLog("done redrawing")
+            for (idx, contact) in contacts.enumerate() {
+                let newTile = ContactTile(
+                    frame: NSRect(origin: NSPoint(x: origin.x, y: origin.y - ((tileSize.height + padding) * CGFloat(idx))), size: tileSize),
+                    contact: contact
+                );
+                self.addSubview(newTile);
+            }
+        });
     }
 
     required init(coder: NSCoder) {
