a56719540596334e52ae0f37553a4b4df686f5c7
Clint Tseng <clint@dontexplain.com>
2016-07-01 15:53:05 -0700

    new: use nsvisualeffectview for blurred message bubbles.
    problem is, this causes a couple visual artifacts:
    * weird dark rect over contact avatar.
    * fade in/out of text looks really bad due to bad antialias calc.

--- sidetalk/view/MessageView.swift
+++ sidetalk/view/MessageView.swift
@@ -3,10 +3,8 @@ import Foundation
 import ReactiveCocoa
 import enum Result.NoError
 
-class MessageView: NSView {
+class MessageView: NSVisualEffectView {
     private let textView: NSTextView;
-    private let bubbleLayer: CAShapeLayer;
-    private var calloutLayer: CAShapeLayer?;
 
     private let textWidth = CGFloat(250);
     private let width: CGFloat;
@@ -14,7 +12,7 @@ class MessageView: NSView {
     private var _outerHeight: CGFloat?;
     var outerHeight: CGFloat { get { return self._outerHeight ?? 0; } };
 
-    private let bubbleColor = NSColor(red: 0.1, green: 0.12, blue: 0.15, alpha: 0.85).CGColor;
+    private let bubbleColor = NSColor(red: 0.1, green: 0.12, blue: 0.15, alpha: 0.35).CGColor;
     private let bubbleMarginX = CGFloat(4);
     private let bubbleMarginY = CGFloat(4);
     private let bubbleRadius = CGFloat(3);
@@ -26,20 +24,19 @@ class MessageView: NSView {
 
     init(frame: NSRect, width: CGFloat, message: Message, conversation: Conversation) {
         self.textView = NSTextView(frame: NSRect(origin: NSPoint.zero, size: NSSize(width: textWidth, height: 0)));
-        self.bubbleLayer = CAShapeLayer();
-        self.calloutLayer = CAShapeLayer();
         self.width = width;
         self.message = message;
         self.conversation = conversation;
 
         super.init(frame: frame);
+
+        self.material = .Dark;
+        self.state = .Active;
     }
 
     override func viewWillMoveToSuperview(newSuperview: NSView?) {
         super.viewWillMoveToSuperview(newSuperview);
         self.wantsLayer = true;
-
-        let foreign = self.message.from == self.conversation.with;
 
         // render text view.
         self.textView.verticallyResizable = true;
@@ -59,54 +56,61 @@ class MessageView: NSView {
         // size it up to fit the text.
         self.textView.sizeToFit();
 
+        // clip ourselves.
+        self.clip(callout: true);
+
+        // add everything.
+        self.addSubview(self.textView);
+    }
+
+    // http://stackoverflow.com/a/29386935
+    @objc dynamic var cornerMask: NSImage?;
+    @objc dynamic func _cornerMask() -> NSImage? { return self.cornerMask; };
+
+    func removeCallout() {
+        self.clip(callout: false);
+    }
+
+    private func clip(callout callout: Bool) {
+        let foreign = self.message.from == self.conversation.with;
+
         // calculate text/bubble frame.
         let size = self.textView.layoutManager!.usedRectForTextContainer(self.textView.textContainer!).size;
         let origin = NSPoint(x: foreign ? (self.width - bubbleMarginX - size.width) : (calloutSize + bubbleMarginX), y: bubbleMarginY);
-        self.textView.setFrameOrigin(origin);
 
         // draw bubble.
         let bubbleRect = NSRect(origin: origin, size: size).insetBy(dx: -bubbleMarginX, dy: -bubbleMarginY);
         let bubblePath = NSBezierPath(roundedRect: bubbleRect, xRadius: bubbleRadius, yRadius: bubbleRadius);
-        self.bubbleLayer.path = bubblePath.CGPath;
-        self.bubbleLayer.fillColor = self.bubbleColor;
 
-        // draw callout.
-        if foreign {
-            let rightEdge = origin.x + size.width + bubbleMarginX;
-            let vlineCenter = origin.y + 7.0;
-            let calloutPts = NSPointArray.alloc(3);
-            calloutPts[0] = NSPoint(x: rightEdge, y: vlineCenter + calloutSize);
-            calloutPts[1] = NSPoint(x: rightEdge + calloutSize, y: vlineCenter);
-            calloutPts[2] = NSPoint(x: rightEdge, y: vlineCenter - calloutSize);
-            let calloutPath = NSBezierPath();
-            calloutPath.appendBezierPathWithPoints(calloutPts, count: 3);
-            self.calloutLayer!.path = calloutPath.CGPath;
-            self.calloutLayer!.fillColor = self.bubbleColor;
-            self.layer!.addSublayer(self.calloutLayer!);
-        }
-
-        // add everything.
-        self.layer!.addSublayer(self.bubbleLayer);
-        self.addSubview(self.textView);
-
-        // remember outer height for other calculations.
+        // HACK: bad place for these lines.
+        self.textView.setFrameOrigin(origin);
         self._outerHeight = bubbleRect.height;
 
-        /*//self.layerUsesCoreImageFilters = true;
+        // create clipping mask.
+        let clip = NSImage(size: self.frame.size, flipped: false) { rect in
+            NSColor.blackColor().set();
+            bubblePath.fill();
 
-         let blurFilter = CIFilter(name: "CIGaussianBlur")!;
-         blurFilter.setDefaults();
-         blurFilter.setValue(NSNumber(double: 8.0), forKey: "inputRadius");
-         self.bubbleLayer.backgroundFilters = [ blurFilter ];
-         //self.bubbleLayer.needsDisplayOnBoundsChange = true;
-         self.bubbleLayer.setNeedsDisplay();*/
+            // draw callout.
+            if foreign && callout {
+                let rightEdge = origin.x + size.width + self.bubbleMarginX;
+                let vlineCenter = origin.y + 7.0;
+                let calloutPts = NSPointArray.alloc(3);
+                calloutPts[0] = NSPoint(x: rightEdge, y: vlineCenter + self.calloutSize);
+                calloutPts[1] = NSPoint(x: rightEdge + self.calloutSize, y: vlineCenter);
+                calloutPts[2] = NSPoint(x: rightEdge, y: vlineCenter - self.calloutSize);
+                let calloutPath = NSBezierPath();
+                calloutPath.appendBezierPathWithPoints(calloutPts, count: 3);
+                calloutPath.fill();
+            }
+
+            return true;
+        };
+        clip.capInsets = NSEdgeInsets(top: bubbleRadius, left: bubbleRadius, bottom: bubbleRadius, right: bubbleRadius);
+        clip.resizingMode = .Tile;
+        self.maskImage = clip;
     }
 
-    func removeCallout() {
-        if self.calloutLayer != nil { self.calloutLayer!.removeFromSuperlayer() };
-        self.calloutLayer = nil;
-    }
-    
     required init(coder: NSCoder) {
         fatalError("vocoder");
     }
