630a0f68a3bcdd1c605ce27090915ee7012de47d
Clint Tseng <clint@dontexplain.com>
2018-11-23 15:02:58 -0800

    noop: replace webview with wkwebview since apple insists.
    * kudos, this was less painful than expected.
    * but it wasn’t painless. instantiation can’t be done in IB because i
      need to set the configuration in order to inject a user script.
      * why can’t i do this after the webview exists but before content is
        loaded?

--- sidetalk/ui/Help.swift
+++ sidetalk/ui/Help.swift
@@ -3,31 +3,38 @@ import Foundation;
 import WebKit;
 import MASShortcut;
 
-class HelpController: NSViewController, WebFrameLoadDelegate {
-    @IBOutlet private var webView: WebView?;
-
+class HelpController: NSViewController {
     override func viewWillAppear() {
         super.viewWillAppear();
 
-        // load up our page in the webview.
-        NSLog(Bundle.main.path(forResource: "help", ofType: "html", inDirectory: "web") ?? "no path");
+        guard let path = Bundle.main.path(forResource: "help", ofType: "html", inDirectory: "web") else { return; }
 
-        if let path = Bundle.main.path(forResource: "help", ofType: "html", inDirectory: "web"), let webView = self.webView {
-            webView.frameLoadDelegate = self;
-            webView.mainFrame.load(URLRequest(url: URL(fileURLWithPath: path)));
-        }
-    }
-
-    func webView(_ webView: WebView!, didClearWindowObject windowObject: WebScriptObject!, for frame: WebFrame!) {
-        // the webview window is ready to receive values; populate it with our current shortcut key.
-        let data = UserDefaults.standard.data(forKey: "globalActivation");
-        let shortcut: MASShortcut = (data == nil) ?
+        // get shortcut/account information:
+        let shortcutData = UserDefaults.standard.data(forKey: "globalActivation");
+        let shortcut: MASShortcut = (shortcutData == nil) ?
             MASShortcut.init(keyCode: 0x31, modifierFlags: NSEvent.ModifierFlags.control.rawValue) :
-            try! NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(data!) as! MASShortcut;
-        windowObject.setValue(shortcut.description, forKey: "globalActivation");
+            try! NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(shortcutData!) as! MASShortcut;
+        let account = UserDefaults.standard.string(forKey: "mainAccount");
 
-        // also populate it with whether the user has an account configured.
-        windowObject.setValue(UserDefaults.standard.string(forKey: "mainAccount"), forKey: "mainAccount");
+        // formulate data injection script, controller, config object:
+        let inject = "window.globalActivation = '\(shortcut.description)'; window.mainAccount = '\(account ?? "")'";
+        let script = WKUserScript(source: inject, injectionTime: .atDocumentStart, forMainFrameOnly: true);
+        let controller = WKUserContentController();
+        controller.addUserScript(script);
+        let wkConfig = WKWebViewConfiguration();
+        wkConfig.userContentController = controller;
+        
+        // create a wkwebview and position it (because you can't use IB to set your webview if you want config for some dumb reason)
+        let webView = WKWebView(frame: .zero, configuration: wkConfig);
+        webView.translatesAutoresizingMaskIntoConstraints = false;
+        self.view.addSubview(webView);
+        self.view.addConstraints([
+            webView.constrain.left == self.view.constrain.left,
+            webView.constrain.top == self.view.constrain.top,
+            webView.constrain.right == self.view.constrain.right,
+            webView.constrain.bottom == self.view.constrain.bottom - 36
+        ]);
+        webView.load(URLRequest(url: URL(fileURLWithPath: path)));
     }
 
     @IBAction func done(_ sender: Any) {
